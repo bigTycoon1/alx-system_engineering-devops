52c688722cf7735a2c805932393fa4f7bc1b3013
bigTycoon1 <mztatycoon@gmail.com>
2024-02-25 03:29:09 -0800

    0x0B. SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!user/bin/env bash
+# using puppet to connect without passwrd
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
